f92bae7a8dc9e39eef595025496d8f5c8140d91c
Cesar Jimenez <cijgr@hotmail.com>
2019-02-18 23:47:36 -0600

    Passport JWT Auth Strategy: completed v-13

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load User model
 const User = require('../../models/User');
@@ -96,5 +97,17 @@ router.post('/login', (req, res) => {
         });
     });
 });
+
+// @route:  GET api/users/current
+// @desc:   Return current user
+// @access: Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // res.json(req.user); // this will return back the actual user object (informative)
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
+})
 
 module.exports = router;
